9f058ab6d4d6d54897207b1d2375dde0bc6a0a3a
Mohammad Javad Nikbakht <javadnikbakht@mail.com>
2023-03-24 03:18:29 +0330

    feat(statics): add static root and media root and url to project settings

--- http_poems/settings.py
+++ http_poems/settings.py
@@ -117,7 +117,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = BASE_DIR / "static"
 STATIC_URL = "/static/"
+
+MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_URL = "/media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
