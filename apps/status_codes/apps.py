73a5a446587a0c6b2681a87a034c7793e10ad8d1
Mohammad Javad Nikbakht <javadnikbakht@mail.com>
2024-09-27 04:55:31 +0330

    feat(status_codes): provide a verbose_name for status_codes app config

--- apps/status_codes/apps.py
+++ apps/status_codes/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
 
 class StatusCodesConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "apps.status_codes"
+    verbose_name = _("کد‌های وضعیت")
